1f0ac804031eff7353abc73ceb10aa9e671327c6
Felix Shelishkevich <feshbiz@gmail.com>
2021-04-25 20:49:14 +0300

    Passport: Routes

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
+use Laravel\Passport\RouteRegistrar;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,7 +28,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         if (!$this->app->routesAreCached()) {
-            Passport::routes(null, ['prefix' => 'api']);
+            Passport::routes(function (RouteRegistrar $router) {
+                $router->forAccessTokens();
+            }, ['prefix' => 'api']);
         }
     }
 }
